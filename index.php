a4f5506ed5d56101b85ee69ba1a8374f75262915
moxuanyuan <moxuanyuan@163.com>
2018-09-19 12:29:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,32 +2,27 @@
 error_reporting(E_ALL);
 use Ifsnop\Mysqldump as IMysqldump;
 
-
+/* access_key用作简单的访问控制 */
 $access_key='91279893';
+/* 数据库参数 */
 $dbname='';
 $db_user='';
 $db_password='';
 
 $filename='db_'.date('Y-m-d');
-
-if(isset($_GET['key']) && $_GET['key']==$access_key)
+if(isset($_GET['access_key']) && $_GET['access_key']==$access_key)
 {
     include_once(dirname(__FILE__) . "/Mysqldump.php"); 
-    
     $dumpSettings = array( 
         'compress' => IMysqldump\Mysqldump::GZIP,
         'add-drop-table'=>true
     );
-    
+
     $dump = new IMysqldump\Mysqldump(
         "mysql:host=localhost;dbname={$dbname}",
         $db_user,
         $db_password,
         $dumpSettings
     );
-    
     $dump->start($filename.'.sql.gz');
 }
-
-
- 
